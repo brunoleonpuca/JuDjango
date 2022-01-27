440262d110fc02d76ee5a950adf797befbdcee4a
Leon <brunopuca1@gmail.com>
2022-01-27 20:46:56 +0100

    Login, register and logout setup

--- djangoTestProject/djangoTestApplication/views.py
+++ djangoTestProject/djangoTestApplication/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Movie
 
 # Create your views here.
@@ -12,8 +14,48 @@ def index(request):
     return render(request, 'index.html', {'movies': movies, 'context': context})
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
 
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already used')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Passwords does not match')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'User does not exist')
+            return redirect('login')
+
+    return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
 
 def counter(request):
     text = request.POST['text']
